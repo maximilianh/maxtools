22dd6c0d7b0e13755fb3f52fea7b6cef228604f6
Max <max@soe.ucsc.edu>
2020-06-11 09:49:21 -0700

    a few more tools for lucas

---
+++ covidCheck.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+directory="/hive/data/genomes/wuhCor1/bed/uniprot/"
+file="covid-19.new.xml"
+uri="ftp://ftp.uniprot.org/pub/databases/uniprot/pre_release/covid-19.xml"
+
+cd "$directory"
+if [ ! -f "$file" ]
+then
+    touch $file --date="2000-01-01"
+fi
+
+touch "$file.time" --reference="$file"
+curl -s -o "$file" -z "$file.time" "$uri"
+
+if [[ "$file" -nt "$file.time" ]]
+then
+    printf "File $file has been updated.\n"
+fi
